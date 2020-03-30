62b3fa963020cb383be3dcbe9d0c94016081303b
nicolasariza <nicolas_ariza@outlook.com>
2020-03-29 21:41:41 -0500

    Updates

---
+++ precache-manifest.4464adcb8e2cc00f508cccb3433b6cde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a2291b1de46539af6ca532204de9c2",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "745b837fffcc875e3131",
+    "url": "/covid/static/css/main.9aa3b8f3.chunk.css"
+  },
+  {
+    "revision": "326855ca9e0d69d96da8",
+    "url": "/covid/static/js/2.05a81e49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid/static/js/2.05a81e49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "745b837fffcc875e3131",
+    "url": "/covid/static/js/main.0a1835c7.chunk.js"
+  },
+  {
+    "revision": "cb89d5983b0aee4c0fd2",
+    "url": "/covid/static/js/runtime-main.f8e3727a.js"
+  }
+]);
