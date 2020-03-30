2548321a4b1ce3df1598fbfcd360e2ebfcb372dc
nicolasariza <nicolas_ariza@outlook.com>
2020-03-29 22:35:26 -0500

    se agrega efecto de carga e iconos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React, { useEffect, useState ,useInterval} from "react";
-
+import React, { useEffect, useState } from "react";
+import Spinner from './components/Spinner';
 function App() {
   const [datos, setDatos] = useState({});
   const [date, setDate] = useState(new Date());
+  const [cargando, setCargando] = useState(true);
   const consultarAPI = async () => {
     const api = await fetch("https://covid19.mathdro.id/api/countries/COL/confirmed");
     const resultado = await api.json();
     setDatos(resultado[0]);
+    console.log("consultando...");
+    
   };
 
   useEffect(() => {
     consultarAPI();
+    setTimeout(() => {
+      setCargando(false);
+    }, 2000);
     setInterval( () => tick(), 1000 );
+    setInterval( () => consultarAPI(), 60000 );
   }, []);
 
   function tick() {
@@ -20,22 +27,22 @@ function App() {
   
   return (
     <div className="container">
-      <div className="App">
         <nav className="navbar navbar-light bg-light">
           <a className="navbar-brand" href="#">
             Covid 19 - Colombia
           </a>
         </nav>
-        <div className="d-flex flex-column text-center mt-5">
+        
+          {cargando?<Spinner />:
+          <div className="d-flex flex-column text-center mt-5">
           <h1 className="display-3">Colombia</h1>
-          <h1 className="mt-4">Confirmados</h1>
+          <h1 className="mt-4"><i className="fa fa-plus-square"></i> Confirmados</h1>
   <h3>{datos.confirmed}</h3>
-  <h1 className="mt-4">Fecha de actulizacion</h1>
+  <h1 className="mt-4"><i className="fa fa-history"></i> Actulizacion</h1>
   <h3>{new Date(datos.lastUpdate).toLocaleString()}</h3>
   <h5 className="mt-4">Fecha actual</h5>
   <p>{date.toLocaleString()}</p>
-        </div>
-      </div>
+        </div>}
     </div>
   );
 }
